ef298003581ed4373011af2a7552d4e2846c1d5e
RuslanUC <dev_ruslan_uc@protonmail.com>
2026-03-11 19:22:14 +0200

    add csv file source for test data

--- src/test/java/com/rdev/izat/lb2/CalculatorTest.java
+++ src/test/java/com/rdev/izat/lb2/CalculatorTest.java
@@ -9,6 +9,7 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.Test;
 
@@ -44,16 +45,7 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "1,2,+,0,Result: 3",
-            "1,2,-,0,Result: -1",
-            "1,2,*,0,Result: 2",
-            "1,2,/,0,Result: 0.500",
-            "2,1,/,0,Result: 2",
-            "-1,2,+,0,Result: 1",
-            "0.1,0.2,*,0,Result: 0.020",
-            "18446744073709551616,18446744073709551616,*,0,Result: 340282366920938463463374607431768211456",
-    })
+    @CsvFileSource(resources = "/test_operations.csv", numLinesToSkip = 1)
     void testOperations(String num1, String num2, String op, int ret, String expected) {
         String input = num1 + "\n" + num2 + "\n" + op + "\n";
         LastLineLogger logger = new LastLineLogger();
@@ -76,16 +68,7 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "1,a,+,Invalid number!",
-            "a,2,-,Invalid number!",
-            "a,b,*,Invalid number!",
-            "1,2,//,Invalid operation!",
-            "inf,2,+,Invalid number!",
-            "1,-inf,+,Invalid number!",
-            "nan,2,+,Invalid number!",
-            "1,-nan,+,Invalid number!"
-    })
+    @CsvFileSource(resources = "/test_invalid_params.csv", numLinesToSkip = 1)
     void testInvalidParams(String num1, String num2, String op, String expected) {
         String input = num1 + "\n" + num2 + "\n" + op + "\n";
         LastLineLogger logger = new LastLineLogger();
